71d902cf70cb1ffc670abf36e578bf47967d6470
sam4ntha <stap0428@gmail.com>
2023-03-23 15:01:44 -0600

    Mandando a llamar cada elemento de menu_opciones.json

--- lib/providers/menu_provider.dart
+++ lib/providers/menu_provider.dart
@@ -1,17 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle;
-//Platilla, cambiar el contenido del menu y maandarla a llamar siempre
+//Plantilla, cambiar el contenido del menu y maandarla a llamar siempre
 class _MenuProvider{
   List<dynamic> options = [];
 
   _MenuProvider(){
-    loadData();
+    //loadData();
   }
 
-  loadData(){
-    rootBundle.loadString('data/menu_opciones.json').then((data){
-      print(data);
-  });
+  // loadData(){
+  //   rootBundle.loadString('data/menu_opciones.json').then((data){
+  //     //print(data); //Tomando como String, se debe convertir a Mapa
+  //     Map dataMap = json.decode(data);
+  //     options = dataMap['rutas'];
+  //     // print(dataMap);
+  //     // print(dataMap['nombreApp']);
+  //     // print(dataMap['rutas']);
+  // });
+  // }
+
+
+Future<List<dynamic>> loadData() async{
+  final respuesta = await rootBundle.loadString('data/menu_opciones.json');
+  Map dataMap = json.decode(respuesta);
+  print(dataMap['rutas']);
+  options = dataMap['rutas'];
+  return options;
   }
-}
+}  
 
   final menuProvider = _MenuProvider();
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:componentes5b/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -15,31 +14,48 @@ class HomeScreen extends StatelessWidget {
       );
   }
 
-
   Widget _lista() {
-    print(menuProvider.options);
-    return ListView(
-      children: 
-        _listaItems(),
-    );
+    // print(menuProvider.options);
+    // menuProvider.loadData().then((options){
+    // print(options);
+    // });
+    return FutureBuilder(
+      future: menuProvider.loadData(),
+      initialData: const [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot){ //Ultima accion con datos en el futuro
+        return ListView(
+           children: _listaItems(snapshot.data as List<dynamic>, context), //Convertirlo a una lista
+        );
+      } ); 
+    
   }
 
-  List<Widget> _listaItems(){
-    return const [
-          ListTile(
-            title: Text('Título'),
-            subtitle: Text('Este es un subtítulo'),
-            leading: Icon(Icons.accessibility),
-            trailing: Icon(Icons.keyboard_arrow_right),
-          ),
-        ListTile(
-            title: Text('Título'),
-            subtitle: Text('Este es un subtítulo'),
-            leading: Icon(Icons.accessibility),
-            trailing: Icon(Icons.keyboard_arrow_right),
-          ),
-        ];
-     
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context){
+    // return const [
+    //       ListTile(
+    //         title: Text('Título'),
+    //         subtitle: Text('Este es un subtítulo'),
+    //         leading: Icon(Icons.accessibility),
+    //         trailing: Icon(Icons.keyboard_arrow_right),
+    //       ),
+    //     ListTile(
+    //         title: Text('Título'),
+    //         subtitle: Text('Este es un subtítulo'),
+    //         leading: Icon(Icons.accessibility),
+    //         trailing: Icon(Icons.keyboard_arrow_right),
+    //       ),
+    //     ];
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final item = ListTile(
+        title: Text(opt['texto']),
+        subtitle: Text(opt['texto2']),
+        leading: const Icon(Icons.accessibility),
+        trailing: const Icon(Icons.keyboard_arrow_right),
+        onTap: (){},
+      );
+      opciones..add(item)..add(const Divider());
+    });
+    return opciones;
   }
-
 }
